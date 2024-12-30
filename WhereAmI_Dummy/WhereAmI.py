0f311f3e90a63b27530d54ee02292076d52bd252
gregk27 <git@gregk.ca>
2024-12-30 14:44:57 -0500

    Improve manual GPS program

--- WhereAmI_Dummy/WhereAmI.py
+++ WhereAmI_Dummy/WhereAmI.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 
 import socketio
@@ -20,6 +21,32 @@ def disconnect():
 
 sock.connect("http://127.0.0.1:5000/")
 
-sock.emit("whereami_update", [{'team': 7, 'x':0, 'y':0}])
+team = int(input("Enter team number: "))
+lastX = 0
+lastY = 0
+while True:
+    text = input("Enter position in format x,y: ")
+    x = None
+    y = None
+    if text == "":
+        x = lastX
+        y = lastY
+    else:
+        try:
+            match = re.match(r"\s*([\d.\-+]+)\s*,\s*([\d.\-+]+)", text)
+            x = float(match.group(1))
+            y = float(match.group(2))
+        except Exception as e:
+            print("Invalid input format")
+
+    if x is None or y is None:
+        continue
+
+    print(f"Setting team {team} to ({x}, {y})")
+
+    sock.emit("whereami_update", [{'team': team, 'x': x, 'y': y}])
+
+    lastX = x
+    lastY = y
 
 sock.disconnect()

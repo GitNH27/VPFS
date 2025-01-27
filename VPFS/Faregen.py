1523bf05c3bb79a06c40ee51da606cebe88ec521
Gregory Kelly <git@gregk.ca>
2025-01-26 22:30:11 -0500

    And random fare type selection to Faregen. Factors adjusted to compensate for current number of fares on top of existing probabilities

--- VPFS/Faregen.py
+++ VPFS/Faregen.py
@@ -22,6 +22,15 @@ points = [
 DIST_MIN = 0.5
 DIST_MAX = 999
 
+# TODO: Find a way to link this to the one in FMS.py without a circular import
+TARGET_FARES = 5
+# Aim to have 1-2 of the special fare types at a time, Normal is the remainder
+targetProbabilities = {
+    FareType.SUBSIDIZED: 1.5 / TARGET_FARES,
+    FareType.SENIOR: 1.5 / TARGET_FARES
+}
+targetProbabilities[FareType.NORMAL] = 1 - sum(targetProbabilities.values())
+
 def generate_fare(existingFares : [Fare]) -> Fare or None:
 
     # May be bound later to guarantee a range of distances
@@ -29,10 +38,19 @@ def generate_fare(existingFares : [Fare]) -> Fare or None:
     max_dist = DIST_MAX
 
     existing = []
+    # Track number of each type
+    quantities: dict[FareType, float] = {
+        FareType.NORMAL: 0,
+        FareType.SUBSIDIZED: 0,
+        FareType.SENIOR: 0,
+    }
+    active_fares : int = 0
     for fare in existingFares:
         if fare.isActive:
+            active_fares += 1
             existing.append(fare.src)
             existing.append(fare.dest)
+            quantities[fare.type] += 1
 
     # Cap at 10 tries to avoid getting stuck
     ovf = 0
@@ -52,7 +70,21 @@ def generate_fare(existingFares : [Fare]) -> Fare or None:
             continue
         success = True
 
+    # Additional probability weightings to try to balance options
+    prob_mul: dict[FareType, float] = { }
+    for key, value in quantities.items():
+        # Determine the current ratio for the fare type, relative to target qty not actual qty
+        curr_ratio = value / max(active_fares, 1)
+        if curr_ratio == 0:
+            curr_ratio = 0.01
+        # Calculate the probability multiplier based on target and current. Bound between 1/10x and 10x
+        prob_mul[key] = min(max(targetProbabilities[key] / curr_ratio, 1/4), 10)
+
     if success:
         prob = FareProbability()
+        # Multiply base probabilities by the
+        for key, value in prob:
+            prob[key] *= prob_mul.get(key, 1)
+
         return Fare(p1, p2, prob.roll())
     return None

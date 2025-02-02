Fix RefTags transformation matrix to set translation entries and rotation to unity

WhereAmI/RefTags.py
@@ -12,9 +12,9 @@ class ReferenceTag:
 
     def _build_mat(self) -> ArrayLike:
         return np.array([
-            [self.x, 0, 0, 0],
-            [0, self.y, 0, 0],
-            [0, 0, self.z, 0],
+            [1, 0, 0, self.x],
+            [0, 1, 0, self.y],
+            [0, 0, 1, self.z],
             [0, 0, 0, 1]
         ])
 
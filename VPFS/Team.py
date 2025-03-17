c0508fd0ae08de86137de2f707206d4119b6b1b7
GitNH27 <nicholas.hum27@gmail.com>
2025-03-17 19:17:45 -0400

    edit team number

--- VPFS/Team.py
+++ VPFS/Team.py
@@ -4,7 +4,7 @@ from Utils import Point
 
 class Team:
     def __init__(self, number : int):
-        self.number = number
+        self.number = 32
         self.money = 0
         self.karma = 20
         self.currentFare : int or None = None
